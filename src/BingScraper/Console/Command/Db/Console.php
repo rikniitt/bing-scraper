c3ba8a3201211e341e3a25793eb01923b272391b
rikniitt <riku.niittymaki@helsinki.fi>
2017-04-27 13:34:01 +0300

    Added db:console command.

---
+++ src/BingScraper/Console/Command/Db/Console.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace BingScraper\Console\Command\Db;
+
+use BingScraper\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+
+class Console extends Command
+{
+    protected function configure()
+    {
+        $this->setName('db:console')
+             ->setDescription('Open database console.');
+    }
+
+    protected function execute(InputInterface $in, OutputInterface $out)
+    {
+        parent::execute($in, $out);
+
+        $container = $this->getContainer();
+        $database = $container['db.file'];
+
+        $this->out->writeln('Opening database console.');
+
+        $cmd = '/usr/bin/sqlite3 -column -header ' . $database;
+        $this->out->writeln(" $ $cmd"); 
+
+        $process = new Process($cmd);
+        try {
+            $process->setTty(true);
+            $process->mustRun(function ($type, $buffer) {});
+        } catch (ProcessFailedException $e) {
+            $this->out->writeln('<error>' . $e->getMessage() . '</error>');
+        }
+    }
+
+}
