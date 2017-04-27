8562c5a037e09f8b55eae9cdb916f77b03d9dc8c
rikniitt <riku.niittymaki@helsinki.fi>
2017-04-27 15:39:44 +0300

    Added hidden alias command db for db:console.

---
+++ src/BingScraper/Console/Command/Db/Db.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BingScraper\Console\Command\Db;
+
+use BingScraper\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
+
+class Db extends Command
+{
+    protected function configure()
+    {
+        $this->setName('db')
+             ->setHidden(true);
+    }
+
+    protected function execute(InputInterface $in, OutputInterface $out)
+    {
+        parent::execute($in, $out);
+
+        $dbConsole = $this->getApplication()->find('db:console');
+        return $dbConsole->run(new ArrayInput([]), $out);
+    }
+
+}
